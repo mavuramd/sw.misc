d0583176946633c7a571456c80e3f2bf98064c25
mavuramd <dilip426426@gmail.com>
2021-02-12 10:08:08 +0100

    updates witrh encruption

--- hass_backup.sh
+++ hass_backup.sh
@@ -5,10 +5,19 @@ timestamp=$(date '+%Y%m%d_%H%M%S')
 backup_filename=hass_$timestamp.tar.gz
 
 # Create Tar.gz
-tar -cvzf $backup_filename /home/homeassistant/.homeassistant/ || exit 1
+tar -cvzf $backup_filename /home/homeassistant/.homeassistant/
 
 # Copy to google drive
-rclone --config=/home/pi/.config/rclone/rclone.conf copy $backup_filename "gdrive:/rclone_backups" || exit 2
+rclone --config=/home/pi/.config/rclone/rclone.conf copy $backup_filename "gdrive:/rclone_backups/hass" || exit 2
 
 # check on google drive
 rclone --config=/home/pi/.config/rclone/rclone.conf ls gdrive:/ | grep $backup_filename || exit 3
+
+# encryption
+cat keyfile | gpg --homedir /home/pi/.gnupg --encrypt --pinentry-mode loopback --yes --sign --armor -r dilip426426@gmail.com --passphrase-fd 0 $backup_filename || exit 4
+
+# Copy to google drive
+rclone --config=/home/pi/.config/rclone/rclone.conf copy $backup_filename.asc "gdrive:/rclone_backups/hass" || exit 4
+
+# check on google drive
+rclone --config=/home/pi/.config/rclone/rclone.conf ls gdrive:/ | grep $backup_filename.asc || exit 6
